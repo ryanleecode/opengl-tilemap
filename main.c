d8cbf7b2f2a73999faf3b1521f3ae59036e7c0fe
Ryan Lee <drdgvhbh@gmail.com>
2020-07-03 11:40:00 -0400

    Add Starter OpenGL Code

--- main.c
+++ main.c
@@ -1,1 +1,52 @@
-int main() { return 0; }
+#include <glad/glad.h>
+//
+#include <GLFW/glfw3.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window =
+        glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Tilemap", NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr, "Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        fprintf(stderr, "Failed to initialize GLAD");
+        return -1;
+    }
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
+
+void framebuffer_size_callback(__attribute__((unused)) GLFWwindow* window,
+                               int width, int height) {
+    glViewport(0, 0, width, height);
+}
